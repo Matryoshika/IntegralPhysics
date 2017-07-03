f7de6a370f53a641dc9b2048c019ba22a2bef614
Matryoshika <jonathancarlsson159@gmail.com>
2017-07-03 06:06:43 +0200

    MATH?! OH NO D:

--- src/main/java/com/IntegralPhysics/IntegralPhysics/Common/Utils/Utils.java
+++ src/main/java/com/IntegralPhysics/IntegralPhysics/Common/Utils/Utils.java
@@ -1,8 +1,8 @@
 package com.IntegralPhysics.IntegralPhysics.Common.Utils;
 
 public class Utils {
-	
-	//Voltage####################################################################
+
+	// Voltage####################################################################
 
 	/**
 	 * Returns voltage from ampere & ohm
@@ -17,12 +17,12 @@ public class Utils {
 	public static double getVoltWA(double ampere, double watt) {
 		return watt / ampere;
 	}
-	
-	public static double getVoltWO(double watt, double ohm){
+
+	public static double getVoltWO(double watt, double ohm) {
 		return Math.sqrt(watt * ohm);
 	}
-	
-	//Ohm########################################################################
+
+	// Ohm########################################################################
 
 	/**
 	 * Returns Ohm from volt & ampere
@@ -37,57 +37,57 @@ public class Utils {
 	public static double getOhmWA(double watt, double ampere) {
 		return watt / Math.pow(ampere, 2);
 	}
-	
+
 	/**
 	 * Returns Ohm from volt and ohm
 	 */
-	public static double getOhmVW(double volt, double watt){
+	public static double getOhmVW(double volt, double watt) {
 		return Math.pow(volt, 2) / watt;
 	}
-	
-	//Ampere#####################################################################
+
+	// Ampere#####################################################################
 
 	/**
 	 * Returns ampere from watt & voltage
 	 */
-	public static double getAmpereWV(double watt, double volt){
+	public static double getAmpereWV(double watt, double volt) {
 		return watt / volt;
 	}
-	
+
 	/**
 	 * Returns ampere from voltage & ohm
 	 */
-	public static double getAmpereVO(double volt, double ohm){
+	public static double getAmpereVO(double volt, double ohm) {
 		return volt / ohm;
 	}
-	
+
 	/**
 	 * Returns ampere from watt & ohm
 	 */
-	public static double getAmpereWO(double watt, double ohm){
+	public static double getAmpereWO(double watt, double ohm) {
 		return Math.sqrt(watt / ohm);
 	}
-	
-	//Watt#######################################################################
-	
+
+	// Watt#######################################################################
+
 	/**
 	 * Returns watt from volt & ampere
 	 */
-	public static double getWattVA(double volt, double ampere){
+	public static double getWattVA(double volt, double ampere) {
 		return volt * ampere;
 	}
-	
+
 	/**
 	 * Returns watt from volt & ohm
 	 */
-	public static double getWattVO(double volt, double ohm){
+	public static double getWattVO(double volt, double ohm) {
 		return Math.pow(volt, 2) / ohm;
 	}
-	
+
 	/**
 	 * Returns watt from ampere & ampere
 	 */
-	public static double getWattAO(double ampere, double ohm){
+	public static double getWattAO(double ampere, double ohm) {
 		return Math.pow(ampere, 2) * ohm;
 	}
 
